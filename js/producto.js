3930246c112d893978e359f937646aa5926ad24c
priscilaGarciaMoya <priscilagarciamoya@alu.frt.utn.edu.ar>
2023-05-01 17:41:00 -0300

    se agrega modificaciones del challenge 33 de js

--- js/producto.js
+++ js/producto.js
@@ -32,7 +32,7 @@ class Producto {
         this.#nombre = precio;
     }
     get imprimirInformacionProducto() {
-        return `Codigo: ${this.#codigo} - Nombre: ${this.#nombre} - Precio: ${this.#precio}`;
+        return `Codigo: ${this.#codigo} - Nombre: ${this.#nombre} - Precio: $ ${this.#precio}`;
     }
     #generaID() {
         let min = Math.ceil(1);
@@ -87,33 +87,55 @@ function listarProductos(producto) {
     fila.appendChild(columna);
 
     buttonDelete.addEventListener("click", (event) => {
-        while(confirm('¿Desa Eliminar el Producto?')){
+        while (confirm('¿Desa Eliminar el Producto?')) {
             let fila = event.target.parentNode.parentNode,
                 codigoProducto = parseInt(fila.getElementsByTagName('td')[0].innerText);
             listado_producto = listado_producto.filter(producto => producto.getCodigo !== codigoProducto);
-            fila.remove(); 
+            fila.remove();
             alert("Producto Eliminado!");
         }
     });
     buttonUpdate.addEventListener("click", () => {
         alert("Error! se encuentra en desarrollo =( ...");
-    }); 
+    });
 
     bodyListaProductos.appendChild(fila);
 }
 
-function calcularTotal(){
+function calcularTotal() {
     document.getElementById("inputNombreProducto").value
     let suma = 0;
-    if(listado_producto.length == 0) return alert('No hay productos en el carrito de compras...'); 
-    document.querySelector("#cantidadProducto").innerText = "  " + listado_producto.length; 
-    listado_producto.forEach(producto => {  
-              suma += producto.getPrecio; 
-    }); 
-    document.querySelector("#totalPrecio").innerText =  "  " + "$" + suma.toFixed(2); 
+    if (listado_producto.length == 0) return alert('No hay productos en el carrito de compras...');
+    document.querySelector("#cantidadProducto").innerText = "  " + listado_producto.length;
+    listado_producto.forEach(producto => {
+        suma += producto.getPrecio;
+    });
+    document.querySelector("#totalPrecio").innerText = "  " + "$" + suma.toFixed(2);
 }
 
-function cancelarOperacion(){
-    window.location.reload(); 
+function cancelarOperacion() {
+    window.location.reload();
 }
-  
+function verFactura() {
+
+}
+document.querySelector('.verFactura').addEventListener('click', function () {
+    const olContenedorPadre = document.getElementById("listadoProductoModal");
+    let sumaModal = 0;
+    while (olContenedorPadre.hasChildNodes()) {
+        olContenedorPadre.childNodes.forEach(li => li.remove());
+    }
+    listado_producto.forEach(producto => {
+        sumaModal += producto.getPrecio;
+        let li = document.createElement("li");
+        li.innerText = producto.imprimirInformacionProducto;
+        olContenedorPadre.appendChild(li);
+    });
+    document.querySelector("#totalPrecioModal").innerText = "  " + "$" + sumaModal.toFixed(2);
+    document.querySelector('#imprimirModal').classList.add('show');
+    document.querySelector('.modal-backdrop').classList.add('show');
+
+});
+function imprimirFacturaProducto() {
+    alert("Error! se encuentra en desarrollo =( ...");
+}
